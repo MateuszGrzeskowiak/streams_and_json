b3e6f4e552f36cc933af3c458a02fd8d008c962b
Mateusz Grzeskowiak <mateusz.r.grzeskowiak@gmail.com>
2020-03-08 10:50:38 +0100

    Initial commmit

---
+++ src/main/java/structures/Osoba.java
@@ -0,0 +1,17 @@
+package structures;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Osoba {
+    private String imie;
+    private String nazwisko;
+    private int wiek;
+
+
+
+}
